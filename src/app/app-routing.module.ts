bd4d297c8057268d2c2b008ed5dd0f636d7a9b7f
Bogdan Carbune <bogdan.carbune@issco.ro>
2023-07-23 15:44:28 +0300

    Create current weather page, forecast page. Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CurrentWeatherComponent } from './pages/current-weather/current-weather.component';
+import { ForecastComponent } from './pages/forecast/forecast.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CurrentWeatherComponent
+  },
+  {
+    path: 'four-days',
+    component: ForecastComponent
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CurrentWeatherComponent } from './pages/current-weather/current-weather.component';
+import { ForecastComponent } from './pages/forecast/forecast.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrentWeatherComponent,
+    ForecastComponent
   ],
   imports: [
     BrowserModule,
