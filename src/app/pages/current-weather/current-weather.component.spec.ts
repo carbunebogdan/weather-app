603e72f9e9a3d8e99d666d186783eb090aa6de6a
Bogdan Carbune <bogdan.carbune@issco.ro>
2023-07-24 07:30:59 +0300

    Tests

--- src/app/pages/current-weather/current-weather.component.spec.ts
+++ src/app/pages/current-weather/current-weather.component.spec.ts
@@ -1,14 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CurrentWeatherComponent } from './current-weather.component';
+import { WeatherService } from 'src/app/services/weather/weather.service';
+import { of } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('CurrentWeatherComponent', () => {
   let component: CurrentWeatherComponent;
   let fixture: ComponentFixture<CurrentWeatherComponent>;
 
+  const mockCurrentWeatherData = {
+    city: 'Mock City',
+    country: 'Mock Country',
+    date: new Date(),
+    weather: { temp: 25, description: 'Mock Weather', icon: 'Mock Icon', tempMax: 30, tempMin: 20 },
+    wind: { speed: 10 },
+    sunrise: new Date(),
+    sunset: new Date(),
+    rain: 0
+  };
+
+  const weatherService = jasmine.createSpyObj('WeatherService', [], { currentWeatherData: of(mockCurrentWeatherData) });
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [CurrentWeatherComponent]
+      declarations: [CurrentWeatherComponent],
+      providers: [
+        { provide: WeatherService, useValue: weatherService }
+      ],
+      imports: [
+        RouterTestingModule
+      ]
     });
     fixture = TestBed.createComponent(CurrentWeatherComponent);
     component = fixture.componentInstance;
@@ -17,5 +39,9 @@ describe('CurrentWeatherComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize currentWeather$', () => {
+    expect(component.currentWeather$).toEqual(weatherService.currentWeatherData);
   });
 });
--- src/app/pages/forecast/forecast.component.spec.ts
+++ src/app/pages/forecast/forecast.component.spec.ts
@@ -1,14 +1,46 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ForecastComponent } from './forecast.component';
+import { DatePipe } from '@angular/common';
+import { of } from 'rxjs';
+import { LocationService } from 'src/app/services/location/location.service';
+import { WeatherService } from 'src/app/services/weather/weather.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ForecastComponent', () => {
   let component: ForecastComponent;
   let fixture: ComponentFixture<ForecastComponent>;
+  let locationService: jasmine.SpyObj<LocationService>;
+  let weatherService: jasmine.SpyObj<WeatherService>;
+  let datePipe: jasmine.SpyObj<DatePipe>;
+
+  const mockCoordinates = { lat: 1, lon: 1 };
+  const mockForecastWeather = {
+    days: [
+      { date: new Date(), weather: { temp: 20, tempMin: 15, tempMax: 25, icon: 'icon1' } },
+      { date: new Date(), weather: { temp: 25, tempMin: 20, tempMax: 30, icon: 'icon2' } }
+    ]
+  };
+
+  locationService = jasmine.createSpyObj('LocationService', ['currentCoordinates']);
+  locationService.currentCoordinates.and.returnValue(mockCoordinates);
+
+  weatherService = jasmine.createSpyObj('WeatherService', ['getForecast']);
+  weatherService.getForecast.and.returnValue(of(mockForecastWeather));
+
+  datePipe = jasmine.createSpyObj('DatePipe', ['transform']);
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ForecastComponent]
+      declarations: [ForecastComponent],
+      providers: [
+        { provide: LocationService, useValue: locationService },
+        { provide: WeatherService, useValue: weatherService },
+        DatePipe
+      ],
+      imports: [
+        RouterTestingModule
+      ]
     });
     fixture = TestBed.createComponent(ForecastComponent);
     component = fixture.componentInstance;
@@ -17,5 +49,53 @@ describe('ForecastComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+
+  it('should get the forecast on initialization', () => {
+    expect(weatherService.getForecast).toHaveBeenCalledWith(mockCoordinates.lon, mockCoordinates.lat);
+  });
+
+  it('should initialize forecast weather and group by day', () => {
+    const today = new Date();
+    const tomorrow = new Date().setDate(new Date().getDate() + 1);
+    const mockForecastWeather = {
+      days: [
+        { date: today, weather: { temp: 20, tempMin: 15, tempMax: 25, icon: 'icon1' } },
+        { date: tomorrow, weather: { temp: 25, tempMin: 20, tempMax: 30, icon: 'icon2' } }
+      ]
+    };
+
+    const expectedGroupedByDays = [{
+      list: [
+        { date: today, weather: { temp: 20, tempMin: 15, tempMax: 25, icon: 'icon1' } }
+      ],
+      day: 'Monday',
+      tempMin: 15,
+      tempMax: 25,
+      average: 20,
+      icon: 'icon1'
+    }, {
+      list: [
+        { date: tomorrow, weather: { temp: 25, tempMin: 20, tempMax: 30, icon: 'icon2' } }
+      ],
+      day: 'Tuesday',
+      tempMin: 20,
+      tempMax: 30,
+      average: 25,
+      icon: 'icon2'
+    }];
+
+    weatherService.getForecast.and.returnValue(of(mockForecastWeather));
+    datePipe.transform.and.returnValue('Monday');
+
+    component.ngOnInit();
+
+    expect(component.forecastWeather$).toBeDefined();
+    expect(component.forecastWeatherGroupedByDay$).toBeDefined();
+
+    component.forecastWeatherGroupedByDay$.subscribe((groupedData: any) => {
+      expect(groupedData).toEqual(expectedGroupedByDays);
+    });
   });
 });
